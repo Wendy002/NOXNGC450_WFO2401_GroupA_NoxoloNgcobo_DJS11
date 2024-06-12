978e22f9c0fb10c1ec3ff70917399feb1718e9d2
Noxolo <wendyngcobo98@gmail.com>
2024-06-12 17:51:13 +0200

    initialise useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = React.useState([]) // initialise state for  blog posts
+  const [error, setError] = React.useState(null) //  initialise state for  error messages
+
+  
+
 
   return (
     <>
-    <h1>hello</h1>
+    <h1 className='text-red-600'>hello</h1>
     </>
   )
 }
