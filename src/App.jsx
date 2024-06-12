d49c477590942db38835e418630b7d0d2ceca67a
Noxolo <wendyngcobo98@gmail.com>
2024-06-12 18:02:18 +0200

    fetch data using useEffect hook to handle side effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,39 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [posts, setPosts] = React.useState([]) // initialise state for  blog posts
+  const [preview, setPreview] = React.useState([]) // initialise state for  podcast preview
   const [error, setError] = React.useState(null) //  initialise state for  error messages
+  
+  
+  React.useEffect(()=>{    //set use effect hook for handling api calls
+    async function addPreview(){
+      try {
+        const response = await fetch('https://podcast-api.netlify.app')          //fetch  data and set it to setposts
+        const data = await response.json()
+        setPreview(data)
 
+        if (!response.ok) {
+            throw Error("Data Fetching Failed")
+        }
+
+      } catch (err) {
+        setError(err)        //set error to err
+        
+      }
+    }
+    addPreview()  // call function
+   }, [])
+
+  if (error) {                 // if error display this message
+    return <h1 className='text-red-600 font-extrabold'>{error.message}</h1>
+  }
   
 
 
   return (
     <>
     <h1 className='text-red-600'>hello</h1>
+    
     </>
   )
 }
