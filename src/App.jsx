dcf79010118c6ee721c41fd84a8a3be29b740de2
Noxolo <wendyngcobo98@gmail.com>
2024-06-13 15:45:38 +0200

    create jsx files and setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css'
-
-function App() {
-  const [previews, setPreviews] = React.useState([]) // initialise state for  podcast preview
-  const [error, setError] = React.useState(null) //  initialise state for  error messages
-  
-  
-  React.useEffect(()=>{    //set use effect hook for handling api calls
-    async function addPreviews(){
-      try {
-        const response = await fetch('https://podcast-api.netlify.app')          //fetch  data and set it to set preview
-        const data = await response.json()
-        setPreviews(data)
-
-        if (!response.ok) {
-            throw Error("Data Fetching Failed")
-        }
-
-      } catch (err) {
-        setError(err)        //set error to err
-        
-      }
-    }
-    addPreviews()  // call function
-   }, [])
-
-  if (error) {                 // if error display this message
-    return <h1 className='text-red-600 font-extrabold'>{error.message}</h1>
-  }
-  
-
-
+import Layout from './Layout components/Layout'
+import SideBar from './Layout components/SideBar'
+import Favourites from './pages/Favourites/Favourites'
+import Home from './pages/Home/Home'
+const App = () => {
   return (
-    <>
-       <ul className='list-none'>
-        {previews.map(preview => (
-          <li key={preview.id}>
-            <h3>{preview.title}</h3>
-            <p>{preview.description}</p>
-            <p><span className='font-bold'>Seasons</span>{preview.seasons}</p>
-            
-
-          </li>
-        ))}
-      </ul>
-
-    </>
+    <div>App</div>
   )
 }
 
