8cd720d3ffde15200510a0c316b46323e5c238f6
Noxolo <wendyngcobo98@gmail.com>
2024-06-13 18:04:50 +0200

    import files in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Layout from './Layout components/Layout.jsx'
-import SideBar from './Layout components/SideBar.jsx'
-import Favourites from './pages/Favourites/Favourites.jsx'
-import ShowPreviews from './pages/Home/ShowPreviews.jsx'
+import Layout from '../Layout components/Layout.jsx'
+import SideBar from '../Layout components/SideBar.jsx'
+import Favourites from './Favourites/Favourites.jsx'
+import ShowPreviews from './Home/ShowPreviews.jsx'
+import ShowPreviewDetails from './Home/ShowPreviewDetails.jsx'
 
 const App = () => {
   return (
     <BrowserRouter>
        <Routes>
-        
+
        </Routes>
     </BrowserRouter>
   )
