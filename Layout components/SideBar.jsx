5cb8fa005072cec18b5d70c6128be2c90c89d5ff
Noxolo <wendyngcobo98@gmail.com>
2024-06-16 17:17:14 +0200

    create mobile view  usestate

--- Layout components/SideBar.jsx
+++ Layout components/SideBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Link, NavLink } from 'react-router-dom'
+import '../assets/favourite-icon.svg'
+import '../assets/home-icon.svg'
+import '../assets/icon.svg'
+import '../assets/search-icon.svg'
+
+
+//name: ----, to:-----, icon:.... 'for links'
 
 const SideBar = () => {
+  const [mobileView, setMobileView] = React.useState(false);
+
   return (
-    <div>SideBar</div>
+    <>
+      
+      
+    </>
   )
 }
 
