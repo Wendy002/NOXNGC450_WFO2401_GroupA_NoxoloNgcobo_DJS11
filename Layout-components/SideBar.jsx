c4941153ed079e10823f56d37dafbd6df0424cad
Noxolo <wendyngcobo98@gmail.com>
2024-06-17 08:55:29 +0200

    initialise files

---
+++ Layout-components/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link, NavLink } from 'react-router-dom'
+import '../assets/favourite-icon.svg'
+import '../assets/home-icon.svg'
+import '../assets/icon.svg'
+import '../assets/search-icon.svg'
+
+
+//name: ----, to:-----, icon:.... 'for links'
+
+const SideBar = () => {
+  const [mobileViewOpen, setMobileViewOpen] = React.useState(false);
+
+  return (
+    <> 
+      <div>
+
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default SideBar
