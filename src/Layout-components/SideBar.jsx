e3c997fe85ff08027496bac3e26cd6ab80cc9d46
Noxolo <wendyngcobo98@gmail.com>
2024-06-18 18:04:18 +0200

    import more icons

---
+++ src/Layout-components/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HiHome, HiSearch, HiHeart,hiclose } from 'react-icons/hi'
+import { IoClose } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
+const SideBar = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SideBar
