2ca5865388a8111b58c2a178f47877c1fdc19d4f
Noxolo <wendyngcobo98@gmail.com>
2024-06-18 23:02:14 +0200

    add styling to sidebar pages

--- src/Layout-components/SideBar.jsx
+++ src/Layout-components/SideBar.jsx
@@ -7,20 +7,23 @@ import { RxHamburgerMenu } from "react-icons/rx";
 const SideBar = () => {
   return (
     <div className='font-sans flex flex-[0.5] flex-col text-white bg-slate-800 h-screen w-1/4 '>
-        <div className='text-[#ff564a] flex items-center justify-center gap-1.5 font-[bold] text-xl mx-0 my-4'>
-            <img src='../assets/icon.svg'/>
-            <p className='font-bold text-2xl'>GenZ<span className='text-white'>cast</span></p>
+        <div className='justify-between flex items-center px-16 w-[86%]'>
+            <div className='text-[#ff564a] flex items-center justify-center gap-1.5 font-[bold] text-xl mx-0 my-4'>
+                <img src='../assets/icon.svg'/>
+                <p className='font-bold text-2xl'>GenZ<span className='text-white'>cast</span></p>
+            </div>
+            <div className=' font-bold text-2xl hidden md:block'><IoClose/></div>
         </div>
-        <div className='hidden md:block'><IoClose/></div>
-        <div>
+       
+        
+        <div className=' text-2xl font-bold px-[4px] md:px-[16px] flex flex-row justify-start items-center gap-[12px] cursor-pointer w-full hover:text-[#ff564a]'>
             <HiHome/>
-            <p>Discover</p>             
+            <p className='py-12 px-0'>Discover</p>             
         </div>
-        <div>
+        <div className='text-2xl font-bold px-[4px] md:px-[16px] flex flex-row justify-start items-center gap-[12px] cursor-pointer w-full hover:text-[#ff564a]'>
             <HiHeart/>
-            <p>Favorites</p> 
+            <p className='py-12 px-0'>Favorites</p> 
         </div>
-
     </div>
   )
 }
