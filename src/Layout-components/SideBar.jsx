30fa16440571211dbae26243484257605d4645df
Noxolo <wendyngcobo98@gmail.com>
2024-06-19 00:16:13 +0200

    update styling for the sidebar

--- src/Layout-components/SideBar.jsx
+++ src/Layout-components/SideBar.jsx
@@ -5,7 +5,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { Link, NavLink } from 'react-router-dom';
 
 
-const SideBar = () => {
+const SideBar = ({setMenuOpen}) => {
 
     const activeStyles = {
         fontWeight: "bold",
@@ -13,16 +13,19 @@ const SideBar = () => {
         color: "#ff564a"
     }
   return (
-    <div className='font-sans flex flex-[0.5] flex-col text-white bg-slate-800 h-screen w-1/4 '>
-        <Link to="/">
-            <div className='justify-between flex items-center px-16 w-[86%]'>
+    <div className='font-sans flex-[0.5] flex flex-col text-white bg-slate-800 h-screen 
+    items-start md:fixed md:z-1000 md:w-full md:max-w-[250px] md:left-0 transition duration-300 ease-in-out box-border'>
+
+        <div className='justify-between flex items-center px-16 w-[86%]'>
+            <Link to="/">
                 <div className='text-[#ff564a] mr-6 flex items-center justify-center gap-1.5 font-[bold] text-xl mx-0 my-4'>
                     <img src='../assets/icon.svg'/>
                     <p className='font-bold text-2xl'>GenZ<span className='text-white'>cast</span></p>
                 </div>
-                <div className=' font-bold text-2xl hidden md:block ml-4'><IoClose/></div>
-            </div>
-        </Link>
+            </Link>   
+            <div className=' font-bold text-2xl hidden md:block ml-4'><IoClose/></div>
+        </div>
+        
       
        
         <nav>
