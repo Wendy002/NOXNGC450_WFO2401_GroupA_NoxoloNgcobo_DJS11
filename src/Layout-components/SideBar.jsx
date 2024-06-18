a968d808978df551f7001c82cd3c55d6207c51f0
Noxolo <wendyngcobo98@gmail.com>
2024-06-18 22:32:11 +0200

    add styling to logo

--- src/Layout-components/SideBar.jsx
+++ src/Layout-components/SideBar.jsx
@@ -6,8 +6,11 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 const SideBar = () => {
   return (
-    <div className=''>
-        <div>GenZcast</div>
+    <div className='font-sans flex flex-[0.5] flex-col text-white bg-slate-800 h-screen w-1/4 '>
+        <div className='text-[#ff564a] flex items-center justify-center gap-1.5 font-[bold] text-xl mx-0 my-4'>
+            <img src='../assets/icon.svg'/>
+            <p className='font-bold text-2xl'>GenZ<span className='text-white'>cast</span></p>
+        </div>
         <div><IoClose/></div>
         <div>
             <HiHome/>
