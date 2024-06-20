7696331a7af3ac97d7d6ac0f3fd128fe12971f6c
Noxolo <wendyngcobo98@gmail.com>
2024-06-20 14:09:33 +0200

    create img and info div

--- src/Discover/ShowDetails.jsx
+++ src/Discover/ShowDetails.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link, useParams, useLocation } from 'react-router-dom'
+import { HiHeart } from 'react-icons/hi'
 
 const ShowDetails = () => {
   const [loading, setLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
   const {id} = useParams()    
   const location = useLocation()        //use params to fetch each show id
-  const [show, setShow] = React.useState(null)   //init state to set show info    
+  const [show, setShow] = React.useState([])   //init state to set show info    
+ 
 
   React.useEffect(() => {
     async function addShowDetail(){
@@ -31,6 +33,8 @@ const ShowDetails = () => {
 
   }, [id])
 
+  
+
   if (error) {                 // if error display this message
     return <h1 className='text-red-600 font-extrabold'>{error.message}</h1>
   }
@@ -38,10 +42,31 @@ const ShowDetails = () => {
     return <h1 className='text-white font-extrabold text-3xl justify-center items-center'>Loading ....</h1>
   }
 
-
+  const search = location.state?.search || "" // back up variable
+  const type = location.state?.type || "all"  
+  // check if there is a show is valid then displays or else show....loading..
+  // @returns image , title, desciption, eps and seasons
   
   return (
-    <div>ShowDetails</div>
+    <>
+      
+      {show && (
+        <div key={id} className=' text-white mt -10 flex gap-8 flex-col md:flex-row md:items-end'>
+          <img className="rounded-md w-48" src={show.image} alt={show.title} />
+          <div className=' rounded-md p-6 bg-black flex flex-col'>
+            <p>Episodes</p>
+            <h1 className='text-5xl font-extrabold mb-4 md:text7xl'>{show.title}</h1>
+            <p>{show.description}</p>
+            <p className='text-yellow-400 font-normal'><span>Last updated: </span>{show.updated.slice(0,10)}</p>
+            <HiHeart className=' font-bold text-4xl absolute flex  items-center bg-stone-400 top-2 right-1.5 p-1.5 rounded-[50%] z-[100]'/>
+          </div>
+          <div></div>
+        </div>   
+        
+      )}
+      
+    
+    </>
   )
 }
 
