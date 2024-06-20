5fa404af29ab5f1453d4b8419116e0282bee3acc
Noxolo <wendyngcobo98@gmail.com>
2024-06-20 12:10:13 +0200

    style basic audio

--- src/Layout-components/AudioPlayer.jsx
+++ src/Layout-components/AudioPlayer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FaPlay } from "react-icons/fa";
+import { FaPauseCircle } from "react-icons/fa";
+import { FaStepForward } from "react-icons/fa";
+import { FaBackwardStep } from "react-icons/fa6";
+
 
 const AudioPlayer = () => {
   return (
-    <div>AudioPlayer</div>
+    <div className='bg-slate-800 h-20 flex justify-between items-center text-white px-4'>
+      audio
+    </div>
   )
 }
 
